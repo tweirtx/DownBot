d49f36860ead7fd70064ee1ebf804fc67ff71ad8
Travis <tweirtx@gmail.com>
2018-01-17 12:44:37 -0600

    More stuff implemented

--- downbot.py
+++ downbot.py
@@ -1,6 +1,6 @@
 #Downbot
 
-import discord, os, json, platform
+import discord, os, json, platform, asyncio
 
 config = {
 	'discord_token': "Put Discord API Token here.",
@@ -24,11 +24,25 @@ else:
     startscript = "startscript.sh"
 
 bot = discord.Client()
+loop = asyncio.AbstractEventLoop()
+
+async def startUp():
+    os.system(startscript)
 
 @bot.event()
 async def on_member_update(self, member):
     configmemberid = member.guild.get_member(config[id_to_watch])
     if member.id == configmemberid.id:
+        task = await loop.call_later(config['time_to_wait'], startUp)
+        loop.create_task(shutdown(task))
         print("Debug: alarm raised")
     else:
         print("Debug: event detected but no alarm raised")
+
+@bot.command()
+async def shutdown(task):
+    print("Debugging: task shut down")
+    await task.cancel()
+
+
+bot.run(config['discord_token'])
