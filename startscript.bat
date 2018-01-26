c963b62e42000e7fde086169d89803abdef71b30
Travis Weir <tweirtx@gmail.com>
2018-01-26 12:44:33 -0600

    Update startscript.bat

--- startscript.bat
+++ startscript.bat
@@ -1,2 +1,4 @@
-cd bot_directory REM Set to the appropriate directory
-py -m dozer REM set to the appropriate bot start command for your system
+cd bot_directory 
+REM Set to the appropriate directory
+py -m dozer 
+REM set to the appropriate bot start command for your system
