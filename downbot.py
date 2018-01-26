c632913aba04882a9b0951fb1e54ac6b4e8b8ce5
Travis <tweirtx@gmail.com>
2018-01-26 11:33:48 -0600

    Deleted broken code

--- downbot.py
+++ downbot.py
@@ -31,20 +31,6 @@ async def startUp():
     os.system(startscript)
 
 @bot.event
-async def on_member_update(before, after):
-    if before.id == config['id_to_watch']:
-        task = loop.call_later(config['time_to_wait'], startUp)
-        await loop.create_task(shutdown(task))
-        print("Debug: alarm raised")
-    else:
-        print("Debug: event detected but no alarm raised")
-
-@bot.command()
-async def shutdown(task):
-    print("Debugging: task shut down")
-    await task.cancel()
-
-@bot.event
 async def on_ready():
     print("Ready")
 
