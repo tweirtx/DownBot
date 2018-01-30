9c8e66ae8c76e00436475b693f789d7309e79160
Travis <tweirtx@gmail.com>
2018-01-30 11:47:58 -0600

    Added some test code

--- startscript.bat
+++
@@ -1,4 +0,0 @@
-cd bot_directory 
-REM Set to the appropriate directory
-py -m dozer 
-REM set to the appropriate bot start command for your system
--- startscript.sh
+++
@@ -1,2 +0,0 @@
-cd /bot/directory/here #Set this to the directory where your bot is
-py -m <bot> #Change this to whatever your bot's start command is
