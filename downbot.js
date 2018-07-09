d1198e6f9b4a87c25c5ffbad3b0e1e43ab7de06e
Travis Weir <tweirtx@gmail.com>
2018-07-09 17:27:02 -0500

    JS Hello World

--- downbot.js
+++ downbot.js
@@ -1,1 +1,15 @@
-asdf
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./CONFIG.json');
+
+client.on('ready', () => {
+    console.log("Ready!");
+});
+
+client.on('message', msg => {
+    if (msg.content == 'hello world') {
+        msg.reply("Hello world!");
+    }
+});
+
+client.login(config.discord_token);
